3c7812f9a7f409130b5fee45c05499c744bf23b5
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-06 20:56:50 +0300

    project

---
+++ src/main/java/com/example/skillTrackr/SkillTrackrApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillTrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skillTrackr/SkillTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillTrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
