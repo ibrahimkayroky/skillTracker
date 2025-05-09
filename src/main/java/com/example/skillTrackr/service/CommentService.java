fa841b422332ab914dd9b6714ca3224f34f08afe
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-09 19:32:32 +0300

    Create CommentService.java

---
+++ src/main/java/com/example/skillTrackr/service/CommentService.java
@@ -0,0 +1,49 @@
+package com.example.skillTrackr.service;
+
+import com.example.skillTrackr.dto.CommentDTO;
+import com.example.skillTrackr.model.Comment;
+import com.example.skillTrackr.model.Skill;
+import com.example.skillTrackr.model.User;
+import com.example.skillTrackr.repository.CommentRepository;
+import com.example.skillTrackr.repository.SkillRepository;
+import com.example.skillTrackr.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final SkillRepository skillRepository;
+
+    public String addComment(Long skillId, String userEmail, CommentDTO commentDTO){
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        Skill skill = skillRepository.findById(skillId)
+                .orElseThrow(() -> new EntityNotFoundException("Skill not found"));
+
+        Comment comment = Comment.builder()
+                .content(commentDTO.getContent())
+                .commentedBy(user)
+                .skill(skill)
+                .commentedAt(LocalDateTime.now())
+                .build();
+
+        commentRepository.save(comment);
+        return "comment added";
+    }
+
+    public List<Comment> getCommentsBySkill(Long skillId) {
+        Skill skill = skillRepository.findById(skillId)
+                .orElseThrow(() -> new EntityNotFoundException("Skill now found"));
+        return commentRepository.findBySkill(skill);
+    }
+}
