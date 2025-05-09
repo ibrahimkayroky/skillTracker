d9b06c3ee25eb2c232c5c576454823367a9532c8
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-09 19:32:46 +0300

    Create LikeRepository.java

---
+++ src/main/java/com/example/skillTrackr/repository/LikeRepository.java
@@ -0,0 +1,15 @@
+package com.example.skillTrackr.repository;
+
+import com.example.skillTrackr.model.Like;
+import com.example.skillTrackr.model.Skill;
+import com.example.skillTrackr.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    boolean existsByLikedByAndSkill(User user, Skill skill);
+    Optional<Like> findByLikedByAndSkill(User user, Skill skill);
+}
