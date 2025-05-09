dd277cc8e160acb59def768c8e6d20aa3a2a57ab
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-09 19:33:03 +0300

    Create LikeController.java

---
+++ src/main/java/com/example/skillTrackr/controller/LikeController.java
@@ -0,0 +1,33 @@
+package com.example.skillTrackr.controller;
+
+import com.example.skillTrackr.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/skills")
+@RequiredArgsConstructor
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping("/{skillId}/like")
+    public ResponseEntity<String> likeSkill
+            (@PathVariable Long skillId,
+             Principal principal){
+        String message = likeService.likeSkill(skillId, principal.getName());
+        return ResponseEntity.ok(message);
+    }
+
+    @DeleteMapping("/{skillId}/unlike")
+    public ResponseEntity<String> unlikeSkill(
+            @PathVariable Long skillId,
+            Principal principal
+    ){
+        String message = likeService.unLikeSkill(skillId, principal.getName());
+        return ResponseEntity.ok(message);
+    }
+}
