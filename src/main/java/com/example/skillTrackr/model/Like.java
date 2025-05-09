d3b966cedc47b509d4eb13e61a11869aaab88d6c
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-09 19:32:22 +0300

    Create Like.java

---
+++ src/main/java/com/example/skillTrackr/model/Like.java
@@ -0,0 +1,32 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "likes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User likedBy;
+
+    @ManyToOne
+    @JoinColumn(name= "skill_id")
+    private Skill skill;
+
+    private LocalDateTime likedAt;
+}
