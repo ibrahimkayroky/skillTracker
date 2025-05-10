97dd6da62022f05cfcf2ed6922eeb228c76dea9f
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-10 22:24:30 +0300

    Create Follow.java

---
+++ src/main/java/com/example/skillTrackr/model/Follow.java
@@ -0,0 +1,33 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Table(name = "follows", uniqueConstraints = {@UniqueConstraint(columnNames = {"follower_id", "following_id"})})
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id")
+    private User follower;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id")
+    private User following;
+
+    private LocalDateTime followedAt;
+}
+
