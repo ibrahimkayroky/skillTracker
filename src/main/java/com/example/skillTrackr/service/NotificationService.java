832f33172cc7d973d323c9fa48c82d7c7cd0a884
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-16 14:36:45 +0300

    Create NotificationService.java

---
+++ src/main/java/com/example/skillTrackr/service/NotificationService.java
@@ -0,0 +1,41 @@
+package com.example.skillTrackr.service;
+
+import com.example.skillTrackr.model.Notification;
+import com.example.skillTrackr.model.User;
+import com.example.skillTrackr.repository.NotificationRepository;
+import com.example.skillTrackr.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+
+    public void sendNotification(Long userId, String message){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("user not found"));
+
+        Notification notification = Notification.builder()
+                .user(user)
+                .createdAt(LocalDateTime.now())
+                .message(message)
+                .isRead(false)
+                .build();
+
+        notificationRepository.save(notification);
+    }
+
+    public List<Notification> getNotificationsForUser(Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("user not found"));
+
+        return notificationRepository.findByUserOrderByCreatedAtDesc(user);
+    }
+
+}
