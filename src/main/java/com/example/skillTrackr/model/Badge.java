a3043c9625c8e43564ebca9e24e468a739fa62cc
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-17 22:33:25 +0300

    Create Badge.java

---
+++ src/main/java/com/example/skillTrackr/model/Badge.java
@@ -0,0 +1,33 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "badges")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime grantedAt;
+
+}
