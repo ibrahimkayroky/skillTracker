e93442851ad30ac382787d56c1a77edd2a28b3ef
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-19 22:46:32 +0300

    Update UserRepository.java

--- src/main/java/com/example/skillTrackr/repository/UserRepository.java
+++ src/main/java/com/example/skillTrackr/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.example.skillTrackr.repository;
 import com.example.skillTrackr.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-
+    List<User> findTop10ByOrderByScoreDesc();
     Optional<User> findById(Long userId);
 }
