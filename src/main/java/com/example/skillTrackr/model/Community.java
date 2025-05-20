6ba1d7dc66d2b908817bf2ed297e4b3b285a3eb9
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-20 22:48:48 +0300

    Create Community.java

---
+++ src/main/java/com/example/skillTrackr/model/Community.java
@@ -0,0 +1,36 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "communities")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Community {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String description;
+
+    private Boolean isPrivate;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+}
