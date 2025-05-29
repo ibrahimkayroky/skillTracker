27b26d19e4d04d207e06248de43d3c761615bf9d
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-29 18:59:53 +0300

    Create Message.java

---
+++ src/main/java/com/example/skillTrackr/model/Message.java
@@ -0,0 +1,37 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private String content;
+
+    private LocalDateTime sentAt;
+
+    private Boolean seen;
+
+}
