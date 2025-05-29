4c539c291616e82e7fe7ee3d3d21e2525d182549
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-29 18:59:59 +0300

    Create MessageDTO.java

---
+++ src/main/java/com/example/skillTrackr/dto/MessageDTO.java
@@ -0,0 +1,14 @@
+package com.example.skillTrackr.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDTO {
+
+    private Long receiverId;
+    private String content;
+}
