ccd7932bbf4787425312a85783e61bd3c318abbb
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-29 19:00:15 +0300

    Create CommunityService.java

---
+++ src/main/java/com/example/skillTrackr/service/CommunityService.java
@@ -0,0 +1,48 @@
+package com.example.skillTrackr.service;
+
+import com.example.skillTrackr.dto.JoinCommunityRequest;
+import com.example.skillTrackr.model.*;
+import com.example.skillTrackr.repository.CommunityMemberRepository;
+import com.example.skillTrackr.repository.CommunityRepository;
+import com.example.skillTrackr.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityService {
+
+    private final CommunityRepository communityRepository;
+    private final CommunityMemberRepository communityMemberRepository;
+    private final UserRepository userRepository;
+
+    public String joinCommunity(JoinCommunityRequest request){
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Community community = communityRepository.findById(request.getCommunityId())
+                .orElseThrow(() -> new RuntimeException("Community not found"));
+
+        if (communityMemberRepository.findByUserIdAndCommunityId(user.getId(), community.getId()).isPresent()){
+            return "user already exists";
+        }
+
+        CommunityMember member = CommunityMember.builder()
+                .user(user)
+                .community(community)
+                .role(CommunityRole.MEMBER)
+                .joinedAt(LocalDateTime.now())
+                .build();
+
+        communityMemberRepository.save(member);
+        return "user joined";
+    }
+
+    public List<CommunityMember> getMembers(Long communityId) {
+        return communityMemberRepository.findByCommunityId(communityId);
+    }
+
+}
