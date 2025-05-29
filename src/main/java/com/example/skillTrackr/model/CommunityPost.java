5a75db655b0297bf44dfe2ed30c59cc51d671ad6
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-29 19:00:26 +0300

    Create CommunityPost.java

---
+++ src/main/java/com/example/skillTrackr/model/CommunityPost.java
@@ -0,0 +1,32 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.management.LockInfo;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "community_posts")
+public class CommunityPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "community_id", nullable = false)
+    private Community community;
+}
