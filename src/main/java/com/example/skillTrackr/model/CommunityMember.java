7d58fd0980c80d286c428f6adc4133ba512e1a51
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-29 19:00:29 +0300

    Create CommunityMember.java

---
+++ src/main/java/com/example/skillTrackr/model/CommunityMember.java
@@ -0,0 +1,37 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name= "community_members", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "community_id"})
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommunityMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "community_id", nullable = false)
+    private Community community;
+
+    @Enumerated(EnumType.STRING)
+    private CommunityRole role;
+
+    private LocalDateTime joinedAt;
+}
