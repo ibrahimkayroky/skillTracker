521f2a9eea7d68e91704e102a6b927479edd19dd
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-30 14:36:29 +0300

    Create PostLike.java

---
+++ src/main/java/com/example/skillTrackr/model/PostLike.java
@@ -0,0 +1,34 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "post_likes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "post_id"})
+})
+public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private CommunityPost post;
+
+    private LocalDateTime likedAt;
+}
