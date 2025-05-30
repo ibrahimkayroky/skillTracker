c432647400e9ad362ba7e28d92dacbd02587675d
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-05-30 15:00:08 +0300

    Create PostComment.java

---
+++ src/main/java/com/example/skillTrackr/model/PostComment.java
@@ -0,0 +1,34 @@
+package com.example.skillTrackr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime commentedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private CommunityPost post;
+}
