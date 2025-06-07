96bacd2ecefed92145911f22fb8906d04103b191
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-06-07 03:38:06 +0300

    Update MessageDTO.java

--- src/main/java/com/example/skillTrackr/dto/MessageDTO.java
+++ src/main/java/com/example/skillTrackr/dto/MessageDTO.java
@@ -1,14 +1,20 @@
 package com.example.skillTrackr.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class MessageDTO {
-
+    private String senderUsername;
     private Long receiverId;
     private String content;
+    private LocalDateTime timestamp;
+
 }
