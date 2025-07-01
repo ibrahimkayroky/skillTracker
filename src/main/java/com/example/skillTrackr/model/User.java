d44f6f2d4f7fb37fd48ad1b79ee9d76425fb4af0
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2025-07-01 21:41:53 +0300

    Update User.java

--- src/main/java/com/example/skillTrackr/model/User.java
+++ src/main/java/com/example/skillTrackr/model/User.java
@@ -15,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
